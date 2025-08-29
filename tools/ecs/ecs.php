ff50bc636b69973963182c6bbf10cd6b2c31ff7d
Benjamin Morel <benjamin.morel@gmail.com>
2025-08-29 18:20:23 +0200

    Add brick/coding-standard

---
+++ tools/ecs/ecs.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import(__DIR__ . '/vendor/brick/coding-standard/ecs.php');
+
+    $libRootPath = realpath(__DIR__ . '/../../');
+
+    $ecsConfig->paths(
+        [
+            $libRootPath . '/src',
+            $libRootPath . '/tests',
+            __FILE__,
+        ],
+    );
+
+    $ecsConfig->skip([
+        'tests/Classes/*.php',
+    ]);
+};
